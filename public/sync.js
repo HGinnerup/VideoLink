7b0759a48361f7035b547fe9a960da517f2071eb
Henrik Ginnerup <hginnerup@hotmail.com>
2023-02-09 13:43:24 +0100

    Refactor

--- public/sync.js
+++ public/sync.js
@@ -2,55 +2,69 @@ const client = new WebSocket(`wss://${location.host}`);
 
 client.onOpen = () => { };
 
+function clientSend(action, data) {
+    client.send(JSON.stringify({"action": action, "data": data}));
+}
 
 const video = document.querySelector("video");
 let doingSyncedAction = false;
+let doingSyncedActionTimer;
+
+client.actionListener = new EventTarget();
 
 client.addEventListener("message", (event) => {
     console.log("Recieved: ", event);
     doingSyncedAction = true;
-
     try {
-        const data = JSON.parse(event.data);
-        console.log(data);
+        const socketPackage = JSON.parse(event.data);
+        console.log(socketPackage);
 
-        switch(data.action) {
-            case "play":
-                video.play();
-                video.currentTime = data.currentTime
-                break;
-
-            case "seek":
-                video.currentTime = data.currentTime
-                break;
-
-            case "pause":
-                video.pause();
-                break;
-        }
+        event = new Event(socketPackage.action);
+        event.data = socketPackage.data;
+        client.actionListener.dispatchEvent(event);
     }
     catch(err) {
-
+        console.error("Unhandled socket message: ", err);
     }
 
-    setTimeout(() => {doingSyncedAction = false;}, 300)
-    
+    clearTimeout(doingSyncedActionTimer);
+    doingSyncedActionTimer = setTimeout(() => {doingSyncedAction = false;}, 300)
 });
+
+client.actionListener.addEventListener("play", (data) => {
+    video.currentTime = data.currentTime;
+    video.play();
+});
+
+client.actionListener.addEventListener("seek", (data) => {
+    video.currentTime = data.currentTime;
+});
+
+client.actionListener.addEventListener("pause", (data) => {
+    video.pause();
+});
+
+client.actionListener.addEventListener("ping", (data) => {
+    clientSend("pong", {})
+});
+
+
 
 video.onplay = () => {
     if(doingSyncedAction) return;
-    client.send(JSON.stringify({"action": "play", currentTime: video.currentTime}));
+    clientSend("play", {currentTime: video.currentTime});
 }
 
 video.onseeked = () => {
     if(doingSyncedAction) return;
-    client.send(JSON.stringify({"action": "seek", currentTime: video.currentTime}));
+    clientSend("seek", {currentTime: video.currentTime});
 }
 
 video.onpause = () => {
     if(doingSyncedAction) return;
-    client.send(JSON.stringify({"action": "pause"}));
+    clientSend("pause", {});
 }
+
 
 
 
